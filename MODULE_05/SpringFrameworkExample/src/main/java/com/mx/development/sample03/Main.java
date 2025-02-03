46b40c837436a679a03023091ffd89913e62b802
Said Olano <josesaid@gmail.com>
2025-02-03 08:37:06 -0600

    s

--- MODULE_05/SpringFrameworkExample/src/main/java/com/mx/development/sample03/Main.java
+++ MODULE_05/SpringFrameworkExample/src/main/java/com/mx/development/sample03/Main.java
@@ -1,11 +1,8 @@
 package com.mx.development.sample03;
 
 import com.mx.development.sample03.domain.Employee;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class Main {
     public static void main(String[] args) {
